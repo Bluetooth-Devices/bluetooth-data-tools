810346547fef58ad3672b5e8b6cdd0630c237536
J. Nick Koston <nick@koston.org>
2022-11-12 20:01:18 -0600

    feat: add gap parser

--- src/bluetooth_data_tools/__init__.py
+++ src/bluetooth_data_tools/__init__.py
@@ -1,10 +1,11 @@
 """Bluetooth data tools."""
 from __future__ import annotations
 
-__version__ = "0.2.0"
-
-
 from struct import Struct
+
+from .gap import BLEGAPType, decode_advertisement_data, parse_advertisement_data
+
+__version__ = "0.2.0"
 
 L_PACK = Struct(">L")
 
@@ -14,6 +15,9 @@ __all__ = [
     "newest_manufacturer_data",
     "human_readable_name",
     "short_address",
+    "BLEGAPType",
+    "decode_advertisement_data",
+    "parse_advertisement_data",
 ]
 
 
