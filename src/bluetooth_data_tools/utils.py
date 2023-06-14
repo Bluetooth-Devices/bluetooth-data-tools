558c93f28ffcc205df4a34be0de963fbaeddfafe
J. Nick Koston <nick@koston.org>
2023-06-14 08:41:15 -1000

    feat: reduce string conversion overhead for bluetooth addresses (#12)

--- src/bluetooth_data_tools/utils.py
+++ src/bluetooth_data_tools/utils.py
@@ -1,18 +1,17 @@
 """Bluetooth utils."""
-
-
 from __future__ import annotations
 
-
-def int_to_bluetooth_address(address: int) -> str:
-    """Convert an integer to a bluetooth address."""
-    mac_hex = f"{address:012X}"
-    return f"{mac_hex[0:2]}:{mac_hex[2:4]}:{mac_hex[4:6]}:{mac_hex[6:8]}:{mac_hex[8:10]}:{mac_hex[10:12]}"  # noqa: E501
-
+from functools import lru_cache
 
 try:
-    from ._utils_impl import (  # type: ignore[no-redef] # noqa: F811 F401
-        _int_to_bluetooth_address as int_to_bluetooth_address,
-    )
+    from ._utils_impl import _int_to_bluetooth_address  # noqa: F811 F401
+
+    int_to_bluetooth_address = lru_cache(maxsize=256)(_int_to_bluetooth_address)
+
 except ImportError:
-    pass
+
+    @lru_cache(maxsize=256)
+    def int_to_bluetooth_address(address: int) -> str:
+        """Convert an integer to a bluetooth address."""
+        mac_hex = f"{address:012X}"
+        return f"{mac_hex[0:2]}:{mac_hex[2:4]}:{mac_hex[4:6]}:{mac_hex[6:8]}:{mac_hex[8:10]}:{mac_hex[10:12]}"  # noqa: E501
