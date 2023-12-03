acb32cbdda02015061f08204308a4f11bd54125c
J. Nick Koston <nick@koston.org>
2023-12-02 20:12:04 -1000

    feat: update benchmark

--- bench/int_to_bluetooth_address.py
+++ bench/int_to_bluetooth_address.py
@@ -1,12 +1,21 @@
 import timeit
 
-from bluetooth_data_tools.utils import _int_to_bluetooth_address
+from bluetooth_data_tools.utils import (
+    _int_to_bluetooth_address,
+    int_to_bluetooth_address,
+)
 
 
-def parse_adv() -> None:
+def uncached_parse() -> None:
     _int_to_bluetooth_address(0)
 
 
+def cached_parse() -> None:
+    int_to_bluetooth_address(0)
+
+
 count = 10000000
-time = timeit.Timer(parse_adv).timeit(count)
-print(f"Parsing {count} bluetooth addresses took {time} seconds")
+time = timeit.Timer(uncached_parse).timeit(count)
+print(f"Parsing {count} bluetooth addresses took {time} seconds (uncached)")
+time = timeit.Timer(cached_parse).timeit(count)
+print(f"Parsing {count} bluetooth addresses took {time} seconds (cached)")
