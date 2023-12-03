1d3eee5dececce4ebc93e1fb7eccdc0741c75230
J. Nick Koston <nick@koston.org>
2023-12-02 20:08:17 -1000

    feat: speed up int_to_bluetooth_address

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -5,3 +5,5 @@ def test_int_to_bluetooth_address():
     assert int_to_bluetooth_address(0) == "00:00:00:00:00:00"
     assert int_to_bluetooth_address(1) == "00:00:00:00:00:01"
     assert int_to_bluetooth_address(0xFFFFFFFFFFFF) == "FF:FF:FF:FF:FF:FF"
+    assert int_to_bluetooth_address(0x123456789ABC) == "12:34:56:78:9A:BC"
+    assert int_to_bluetooth_address(0xDEF012345678) == "DE:F0:12:34:56:78"
