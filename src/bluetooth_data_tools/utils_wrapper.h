ac354ae68b7afedfe5242ec50a209a6241d33169
J. Nick Koston <nick@koston.org>
2023-12-02 20:25:39 -1000

    feat: speed up int_to_bluetooth_address (#39)

--- src/bluetooth_data_tools/utils_wrapper.h
+++ src/bluetooth_data_tools/utils_wrapper.h
@@ -4,8 +4,23 @@
 #include <stdio.h>
 
 void _uint64_to_bdaddr(uint64_t address, char *bdaddr) {
-    snprintf(bdaddr, 18, "%02X:%02X:%02X:%02X:%02X:%02X", (uint8_t) (address >> 40) & 0xff,
-                       (uint8_t) (address >> 32) & 0xff, (uint8_t) (address >> 24) & 0xff,
-                       (uint8_t) (address >> 16) & 0xff, (uint8_t) (address >> 8) & 0xff,
-                       (uint8_t) (address >> 0) & 0xff);
+    static const char hex_table[] = "0123456789ABCDEF";
+    *bdaddr++ = hex_table[(address >> 44) & 0x0F];
+    *bdaddr++ = hex_table[(address >> 40) & 0x0F];
+    *bdaddr++ = ':';
+    *bdaddr++ = hex_table[(address >> 36) & 0x0F];
+    *bdaddr++ = hex_table[(address >> 32) & 0x0F];
+    *bdaddr++ = ':';
+    *bdaddr++ = hex_table[(address >> 28) & 0x0F];
+    *bdaddr++ = hex_table[(address >> 24) & 0x0F];
+    *bdaddr++ = ':';
+    *bdaddr++ = hex_table[(address >> 20) & 0x0F];
+    *bdaddr++ = hex_table[(address >> 16) & 0x0F];
+    *bdaddr++ = ':';
+    *bdaddr++ = hex_table[(address >> 12) & 0x0F];
+    *bdaddr++ = hex_table[(address >> 8) & 0x0F];
+    *bdaddr++ = ':';
+    *bdaddr++ = hex_table[(address >> 4) & 0x0F];
+    *bdaddr++ = hex_table[address & 0x0F];
+    *bdaddr++ = '\0';
 }
