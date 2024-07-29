4d8f9a8024f332c012c4f6417d448da0e9a3139b
Aarni Koskela <akx@iki.fi>
2024-07-29 18:09:38 +0300

    fix: speed up int_to_bluetooth_address C implementation

--- src/bluetooth_data_tools/_utils_impl.pyx
+++ src/bluetooth_data_tools/_utils_impl.pyx
@@ -1,22 +1,12 @@
-import cython
+# cython: language_level=3, c_string_type=str, c_string_encoding=ascii
 
-from libc.stdlib cimport malloc
-
-
-cdef extern from "stdint.h":
-    ctypedef unsigned long long uint64_t
+from libc.stdint cimport uint64_t
 
 cdef extern from "utils_wrapper.h":
-    void _uint64_to_bdaddr(uint64_t addr, char *bdaddr)
-
-cdef char* uint64_to_bdaddr(uint64_t address):
-    cdef char* bdaddr = <char *> malloc((18) * sizeof(char))
-    if not bdaddr:
-        return NULL  # malloc failed
-    _uint64_to_bdaddr(address, bdaddr)
-    return bdaddr
-
+    void _uint64_to_bdaddr(uint64_t address, char bdaddr[17]) nogil
 
 
 def _int_to_bluetooth_address(addr: int) -> str:
-    return uint64_to_bdaddr(addr).decode('ascii')
+    cdef char bdaddr[17]
+    _uint64_to_bdaddr(<uint64_t>addr, bdaddr)
+    return <str>bdaddr[:17]
