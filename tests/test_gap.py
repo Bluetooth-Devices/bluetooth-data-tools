13c5a356a6aaa301a740c193d4399c6b2dc8de97
J. Nick Koston <nick@koston.org>
2025-03-09 21:51:48 -1000

    chore: more cover

--- tests/test_gap.py
+++ tests/test_gap.py
@@ -420,6 +420,50 @@ def test_out_of_bounds_length_by_one():
     )
 
 
+def test_manufacturer_data_short():
+    """Test short manufacturer data."""
+
+    data = (b"\x02\xff\x01",)
+
+    adv = parse_advertisement_data(data)
+
+    assert adv.local_name is None
+    assert adv.service_uuids == []
+    assert adv.service_data == {}
+    assert adv.manufacturer_data == {}
+    assert adv.tx_power is None
+
+    assert parse_advertisement_data_tuple(tuple(data)) == (
+        None,
+        [],
+        {},
+        {},
+        None,
+    )
+
+
+def test_service_data_short():
+    """Test short service data."""
+
+    data = (b"\x02\x16\x01",)
+
+    adv = parse_advertisement_data(data)
+
+    assert adv.local_name is None
+    assert adv.service_uuids == []
+    assert adv.service_data == {}
+    assert adv.manufacturer_data == {}
+    assert adv.tx_power is None
+
+    assert parse_advertisement_data_tuple(tuple(data)) == (
+        None,
+        [],
+        {},
+        {},
+        None,
+    )
+
+
 def test_zero_padded_end():
     """Test name with zero padded end."""
 
