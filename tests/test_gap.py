31407d655914edfeebc6f771b8d20a5bb36b2796
J. Nick Koston <nick@koston.org>
2025-04-27 14:47:13 -0500

    chore: coverage

--- tests/test_gap.py
+++ tests/test_gap.py
@@ -233,6 +233,42 @@ def test_parse_advertisement_data_128bit_service_data():
     assert adv.tx_power is None
 
 
+def test_parse_advertisement_data_32bit_and_128bit_service_data():
+    data = [
+        b"\x07\x20\x1a\x02\n\x05\n\xff",
+        b"\x12\x21\x1a\x02\n\x05\n\xff\x062k\x03R\x00\x01\x04\t\x00\x04",
+    ]
+
+    adv = parse_advertisement_data(data)
+
+    assert adv.local_name is None
+    assert adv.service_uuids == []
+    assert adv.service_data == {
+        "00090401-0052-036b-3206-ff0a050a021a": b"\x04",
+        "050a021a-0000-1000-8000-00805f9b34fb": b"\n\xff",
+    }
+    assert adv.manufacturer_data == {}
+    assert adv.tx_power is None
+
+
+def test_parse_advertisement_data_128bit_and_32bit_service_data():
+    data = [
+        b"\x12\x21\x1a\x02\n\x05\n\xff\x062k\x03R\x00\x01\x04\t\x00\x04",
+        b"\x07\x20\x1a\x02\n\x05\n\xff",
+    ]
+
+    adv = parse_advertisement_data(data)
+
+    assert adv.local_name is None
+    assert adv.service_uuids == []
+    assert adv.service_data == {
+        "00090401-0052-036b-3206-ff0a050a021a": b"\x04",
+        "050a021a-0000-1000-8000-00805f9b34fb": b"\n\xff",
+    }
+    assert adv.manufacturer_data == {}
+    assert adv.tx_power is None
+
+
 def test_parse_advertisement_data_128bit_service_data_tuple():
     data = (b"\x12\x21\x1a\x02\n\x05\n\xff\x062k\x03R\x00\x01\x04\t\x00\x04",)
 
