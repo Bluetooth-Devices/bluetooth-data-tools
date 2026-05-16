82513e70cf3ccfa3e9d84c6db3502aaea6963db6
J. Nick Koston <nick@home-assistant.io>
2026-05-16 14:57:16 -0700

    perf(build): parallelize cython extension compilation

--- build_ext.py
+++ build_ext.py
@@ -45,6 +45,8 @@ EXTENSIONS = [
 
 class BuildExt(build_ext):
     def build_extensions(self) -> None:
+        if self.parallel is None:  # type: ignore[has-type, unused-ignore]
+            self.parallel = os.cpu_count() or 1
         try:
             super().build_extensions()
         except Exception:  # noqa: S110
